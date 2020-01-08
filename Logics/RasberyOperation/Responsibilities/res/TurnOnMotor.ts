34dc19c1a4923da5f26d09ac67ae016150821635
Boushaba Mohamed <med1998yz@gmail.com>
2020-01-08 01:05:13 +0100

    add turnonMotor strategy for member

---
+++ Logics/RasberyOperation/Responsibilities/res/TurnOnMotor.ts
@@ -0,0 +1,80 @@
+import { userInfo } from 'os';
+import { RasberyResponsabilities } from './../containers/RasberyResponsabilities';
+import { RasberySql } from './../../../../Mysql/RasberySQL';
+
+import { User } from '../../../../Mysql/User';
+
+import { Observable, Observer } from 'rxjs';
+import { Request, ParamsDictionary, Response } from 'express-serve-static-core';
+export class TurnOnMotor implements RasberyResponsabilities {
+    private Nextchaine!: RasberyResponsabilities;
+    private request: Request<ParamsDictionary>;
+    private response: Response<any>;
+    private data: any;
+
+
+    constructor(request: Request<ParamsDictionary, any, any>, response: Response<any>, data: any) {
+        this.request = request;
+        this.response = response;
+        this.data = data;
+    }
+
+    public setNextChaine(chaine: RasberyResponsabilities): void {
+        this.Nextchaine = chaine;
+    }
+    public processOperation(): Promise<boolean> {
+
+        return new Promise((resolve, reject) => {
+            this.process().subscribe(
+                (resp) => {
+                    if (resp) {
+                        console.log(' Update data achieved an response');
+                        resolve(true);
+                    }
+                },
+                (err) => {
+                    console.log('Error in update data');
+                    reject(false);
+
+                },
+                () => {
+                    console.log('update data complete');
+                }
+            )
+        });
+
+
+    }
+
+    public process(): Observable<boolean> {
+        return new Observable((observer: Observer<boolean>) => {
+
+            console.log("::::::::::::::::::::::::::turn on the Motor:::::::::::::::::::::::::::::::")
+
+            if (this.Nextchaine != null) {
+                console.log('going to next chaine');
+                this.Nextchaine.processOperation()
+                    .then((resp) => {
+                        console.log(resp);
+                        observer.next(true);
+                        observer.complete();
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                        console.log('Error');
+                        observer.error(false);
+                    });
+            } else {
+                console.log('this is the end of the chaine');
+                observer.next(true);
+                observer.complete();
+            }
+
+        });
+
+
+    }
+
+
+
+}
---
+++ Logics/RasberyOperation/Strategy/Strategies/RasberyTurnOnMotorMember.ts
@@ -0,0 +1,67 @@
+import { TurnOnMotor } from './../../Responsibilities/res/TurnOnMotor';
+import { InvitationValidation } from '../../Responsibilities/res/InvitationValidation';
+import { SendEmailInvitation } from '../../Responsibilities/res/SendEmailInvitation';
+import { UpdateData } from '../../Responsibilities/res/UpdateData';
+import { RasberyResponsabilities } from '../../Responsibilities/containers/RasberyResponsabilities';
+import { FactoryStatus } from '../../Responsibilities/res/FactoryStatus';
+import { RequestVerification } from '../../Responsibilities/res/RequestVerification';
+import { UserToRasbery } from '../../Responsibilities/res/UserToRasbery';
+import { RasberyisBlocked } from '../../Responsibilities/res/RabseryisBlocked';
+import { FactoryAuthority } from '../../Responsibilities/res/FactoryAuthority';
+import { UserisReady } from '../../Responsibilities/res/UserisReady';
+import { TokenVerification } from '../../Responsibilities/res/TokenVerification';
+import { RasberyStrategy } from '../containers/RasberyStrategy';
+
+
+import { Request, ParamsDictionary, Response } from 'express-serve-static-core';
+
+export class RasberyTurnOnMotorMember implements RasberyStrategy {
+    private chaine1!: RasberyResponsabilities;
+    private request: Request<ParamsDictionary>;
+    private response: Response<any>;
+    private data: {
+        username: string,
+        email: string,
+    };
+
+    constructor(request: Request<ParamsDictionary, any, any>, response: Response<any>) {
+        this.data = {
+            username: "",
+            email: "",
+        };
+        console.log(request.body);
+        this.request = request;
+        this.response = response;
+        this.chaine1 = new TokenVerification(request, response, this.data);
+        const chaine2 = new UserisReady(request, response, this.data);
+        const chaine3 = FactoryAuthority.getAuthority(request, response, this.data, 'member', 'check');
+        const chaine4 = new TurnOnMotor(request, response, this.data);
+
+
+    }
+
+    public processOperation(): Promise<boolean> {
+        console.log("Rasbery invitation validation startegy");
+        return new Promise((resolve, reject) => {
+
+            this.chaine1.processOperation()
+                .then((resp) => {
+                    if (resp) {
+                        console.log('succes in invitation validation strategy');
+                        resolve(true);
+                    } else {
+                        console.log('error in invitation validation strategy');
+                        reject(false);
+                    }
+
+                })
+                .catch((err) => {
+                    console.log('error in invitation validation strategy');
+                    reject(false);
+                })
+
+
+        });
+
+    }
+}
