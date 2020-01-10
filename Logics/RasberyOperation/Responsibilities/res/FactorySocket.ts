c8299fbd8ebb6d0fb29e5d2aefccbb875ff0509d
Boushaba Mohamed <med1998yz@gmail.com>
2020-01-10 01:07:26 +0100

    fatory config runtime

---
+++ Logics/RasberyOperation/Responsibilities/res/FactorySocket.ts
@@ -0,0 +1,30 @@
+import { SocketWait } from './../Factories/Socket/SocketWait';
+import { SocketTurnOff } from './../Factories/Socket/SocketTurnOff';
+import { SocketTurnOn } from './../Factories/Socket/SocketTurnOn';
+import { RasberyResponsabilities } from './../containers/RasberyResponsabilities';
+import { TurnOff } from './../Factories/RasberyStatus/TurnOff';
+import { TurnOn } from './../Factories/RasberyStatus/TurnOn';
+import { AdminChange } from '../Factories/Authority/AuthorityChange/AdminChange';
+import { GuestChange } from '../Factories/Authority/AuthorityChange/GuestChange';
+import { AdminCheck } from '../Factories/Authority/AuthorityCheck/AdminCheck';
+import { GuestCheck } from '../Factories/Authority/AuthorityCheck/GuestCheck';
+import { Request, Response, ParamsDictionary } from 'express-serve-static-core';
+
+
+
+export class FactoryStatus {
+
+    public static setStatus(request: Request<ParamsDictionary>, response: Response<any>
+        , data: any, operation: string): RasberyResponsabilities {
+
+        if (operation == 'turnon') {
+            return new SocketTurnOn(request, response, data);
+        } else if (operation == 'turnoff') {
+            return new SocketTurnOff(request, response, data);
+        } else if (operation == 'wait') {
+            return new SocketWait(request, response, data);
+        }
+        throw new Error('no Factory selected');
+
+    }
+}
