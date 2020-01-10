cd8d3c092826f354e5345b111101c226a0bbe6ed
Boushaba Mohamed <med1998yz@gmail.com>
2020-01-10 01:04:49 +0100

    fact wait runtime

---
+++ Logics/RasberyOperation/Responsibilities/Factories/Socket/SocketWait.ts
@@ -0,0 +1,85 @@
+import { SocketInfo } from './../../../../../proprieties/SocketInfo';
+import { RasberyResponsabilities } from './../../containers/RasberyResponsabilities';
+
+
+import { Observable, Observer } from 'rxjs';
+import { Request, ParamsDictionary, Response } from 'express-serve-static-core';
+export class SocketWait implements RasberyResponsabilities {
+    private Nextchaine!: RasberyResponsabilities;
+    private request: Request<ParamsDictionary>;
+    private response: Response<any>;
+    private data: any;
+
+
+    constructor(request: Request<ParamsDictionary, any, any>, response: Response<any>, data: any) {
+        this.request = request;
+        this.response = response;
+        this.data = data;
+    }
+
+    public setNextChaine(chaine: RasberyResponsabilities): void {
+        this.Nextchaine = chaine;
+    }
+    public processOperation(): Promise<boolean> {
+
+        return new Promise((resolve, reject) => {
+            this.process().subscribe(
+                (resp) => {
+                    if (resp) {
+                        console.log('Socket wait response');
+                        resolve(true);
+                    }
+                },
+                (err) => {
+                    console.log('Error in socket wait');
+                    reject(false);
+
+                },
+                () => {
+                    console.log('socket wait complete');
+                }
+            )
+        });
+
+
+    }
+
+    public process(): Observable<boolean> {
+        return new Observable((observer: Observer<boolean>) => {
+
+            SocketInfo.getInstance()
+                .getSocket().emit('wait');
+            if (this.Nextchaine != null) {
+                console.log('going to next chaine');
+                this.Nextchaine.processOperation()
+                    .then((resp) => {
+                        console.log(resp);
+                        observer.next(true);
+                        observer.complete();
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                        console.log('Error');
+                        observer.error(false);
+                    });
+            } else {
+                console.log('this is the end of the chaine');
+                observer.next(true);
+                observer.complete();
+            }
+
+        });
+
+
+    }
+
+
+
+
+}
+
+
+/*
+
+
+*/
